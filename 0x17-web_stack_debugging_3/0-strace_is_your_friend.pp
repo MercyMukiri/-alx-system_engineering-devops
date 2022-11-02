70e25cdaacd0aab9f5954b3063c2376a36ddf84d
Mercy Mukiri <mwmukiri@gmail.com>
2022-11-02 14:56:06 -0700

    Add files README.md and 0-strace_is_your_friend.pp

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Fixes error 500 on wordpress runnig on Apache2
+
+exec { 'fix error 500':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php; sudo service apache2 restart',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
+}
