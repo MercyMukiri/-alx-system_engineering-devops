961831ba714123375ff2396374245ec684fd7160
Mercy Mukiri <mwmukiri@gmail.com>
2022-11-03 10:20:08 -0700

    Add file 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,9 +6,15 @@ import requests
 def number_of_subscribers(subreddit):
     """ queries the Reddit API and returns the number of subscribers"""
 
+    if subreddit is None or type(subreddit) is not str:
+        return 0
+
     res = requests.get("https://www.reddit.com/r/{}/about.json"
-                       .format(subreddit))
-    if res.status_code >= 300:
+                       .format(subreddit),
+                       headers={'User-agent': 'custom'},
+                       allow_redirects=False)
+
+    if res.status_code == 404:
         return 0
 
     return res.json().get("data").get("subscribers")
